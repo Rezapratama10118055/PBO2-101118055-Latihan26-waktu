e46f6f0e89fd7a194913524dc1569abbe1d3227f
User <User@DESKTOP-2RC2UP5>
2019-10-07 20:33:19 +0700

    first commit

---
+++ src/pbo2/pkg10118055/latihan26/waktu/PBO210118055Latihan26Waktu.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118055.latihan26.waktu;
+
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class PBO210118055Latihan26Waktu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Date waktu = new Date();
+        System.out.println("Hari ini adalah :"+waktu); 
+    }
+    
+}
